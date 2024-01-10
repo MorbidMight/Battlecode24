0799c54fb70fb609c148aed7b77cfef1344c1802
slifland <sethlifland11@gmail.com>
2024-01-10 10:39:51 -0500

    soldiers attack lowest health possible target

--- src/Version1Center/RobotPlayer.java
+++ src/Version1Center/RobotPlayer.java
@@ -342,8 +342,10 @@ public strictfp class RobotPlayer {
         //attack
         RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
         RobotInfo[] allyRobots = rc.senseNearbyRobots(-1, rc.getTeam());
-        if (enemyRobots.length > 0 && rc.canAttack(enemyRobots[0].location)){
-            rc.attack(enemyRobots[0].location);
+        if (enemyRobots.length > 0){
+            MapLocation toAttack = lowestHealth(enemyRobots);
+            if(rc.canAttack(toAttack))
+                rc.attack(toAttack);
         }
         if(enemyRobots.length == 0 && allyRobots.length > 0){
             for (RobotInfo allyRobot : allyRobots) {
@@ -491,6 +493,18 @@ public strictfp class RobotPlayer {
         }
         int[] coords = {(int)x, (int)y};
         return coords;
+    }
+
+    public static MapLocation lowestHealth(RobotInfo[] enemies){
+        int lowHealth = enemies[0].health;
+        MapLocation toAttack = enemies[0].getLocation();
+        for(RobotInfo enemy : enemies){
+            if(enemy.health < lowHealth){
+                lowHealth = enemy.health;
+                toAttack = enemy.location;
+            }
+        }
+        return toAttack;
     }
 
 }
