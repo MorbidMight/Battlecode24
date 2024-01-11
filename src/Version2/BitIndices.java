ee04bd59884ca90a9cee0e69130bc61d509b1690
uravtanna <uravtanna@gmail.com>
2024-01-10 19:44:04 -0500

    typo lol

--- src/Version2/BitIndices.java
+++ src/Version2/BitIndices.java
@@ -1,6 +1,6 @@
 package Version2;
 
-public class BitIndicies {
+public class BitIndices {
     public static final int soldier_flag_3 = 1023;
     public static final int soldier_flag_2 = 1022;
     public static final int soldier_flag_1 = 1021;
