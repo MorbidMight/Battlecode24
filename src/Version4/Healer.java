5a4de04ce2db5117291053022dca40315b941286
uravtanna <uravtanna@gmail.com>
2024-01-12 11:25:17 -0500

    added version4

---
+++ src/Version4/Healer.java
@@ -0,0 +1,11 @@
+package Version4;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Healer
+{
+    public static void runHealer(RobotController rc) throws GameActionException {
+
+    }
+}
