f24d80294720c16f5a4c294282b0175ea00834ab
Todd <todd.mcknight06@gmail.com>
2024-01-15 15:28:22 -0500

    Builders build explosion bombs if we have above a threshold of crumbs. average robot location know returns null if the the array is null instead of throwing a divide by zero error

--- src/Version7/Builder.java
+++ src/Version7/Builder.java
@@ -6,54 +6,50 @@ import static Version7.RobotPlayer.*;
 
 public class Builder {
     public static void runBuilder(RobotController rc) throws GameActionException {
-        if(turnCount == 150){
-            for(int i = 6; i < 28; i++){
+        if (turnCount == 150) {
+            for (int i = 6; i < 28; i++) {
                 Utilities.clearTask(rc, i);
             }
         }
-        if(!rc.isSpawned()) {
+        if (!rc.isSpawned()) {
             Clock.yield();
         }
         Task t = Utilities.readTask(rc);
         if (SittingOnFlag) {
             //sitting where flag should be, but cant see any flags...
             //if we still cant see a flag 50 turns later, then until we do see one we're gonna assume this location should essentially be shut down
-            if(rc.senseNearbyFlags(-1, rc.getTeam()).length == 0){
+            if (rc.senseNearbyFlags(-1, rc.getTeam()).length == 0) {
                 //shut down this spawn location for now
-                if(countSinceSeenFlag > 40){
+                if (countSinceSeenFlag > 40) {
                     rc.setIndicatorString("Dont come help me!");
                     Clock.yield();
-                }
-                else{
+                } else {
                     countSinceSeenFlag++;
                 }
+            } else {
+                countSinceSeenFlag = 0;
             }
-            else{
-                countSinceSeenFlag=0;
-            }
-            if(countSinceLocked !=0){
+            if (countSinceLocked != 0) {
                 countSinceLocked++;
             }
-            if(countSinceLocked >= 20){
+            if (countSinceLocked >= 20) {
                 countSinceLocked = 0;
                 Utilities.editBitSharedArray(rc, 1021, false);
             }
             //check if nearby enemies are coming to attack, call for robots to prioritize spawning at ur flag
-            if(rc.senseNearbyRobots(-1, rc.getTeam().opponent()).length > rc.senseNearbyRobots(-1, rc.getTeam()).length){
+            if (rc.senseNearbyRobots(-1, rc.getTeam().opponent()).length > rc.senseNearbyRobots(-1, rc.getTeam()).length) {
                 //spawn everyone in 0-8 if possible, also lock so it wont cycle
-                if(rc.getLocation().equals(Utilities.convertIntToLocation(rc.readSharedArray(0)))){
+                if (rc.getLocation().equals(Utilities.convertIntToLocation(rc.readSharedArray(0)))) {
                     Utilities.editBitSharedArray(rc, 1022, false);
                     Utilities.editBitSharedArray(rc, 1023, false);
                     //lock
                     Utilities.editBitSharedArray(rc, 1021, true);
-                }
-                else if(rc.getLocation().equals(Utilities.convertIntToLocation(rc.readSharedArray(1)))){
+                } else if (rc.getLocation().equals(Utilities.convertIntToLocation(rc.readSharedArray(1)))) {
                     Utilities.editBitSharedArray(rc, 1022, false);
                     Utilities.editBitSharedArray(rc, 1023, true);
                     //lock
                     Utilities.editBitSharedArray(rc, 1021, true);
-                }
-                else{
+                } else {
                     Utilities.editBitSharedArray(rc, 1022, true);
                     Utilities.editBitSharedArray(rc, 1023, false);
                     //lock
@@ -76,27 +72,32 @@ public class Builder {
             }*/
         } else if (t != null) {//there is a task to do
             Pathfinding.bugNav2(rc, t.location);
+            TrapType toBeBuilt = TrapType.STUN;
+            if (ShouldBuildWater(rc))
+                toBeBuilt = TrapType.WATER;
+            else if (rc.getCrumbs() >= 3000)
+                toBeBuilt = TrapType.EXPLOSIVE;
             if (locationIsActionable(rc, t.location)) {
-                if (rc.canBuild(TrapType.STUN, t.location)) {
-                    rc.build(TrapType.STUN, t.location);
-                    Utilities.clearTask(rc, t.arrayIndex);
-                }
+                if (rc.canBuild(toBeBuilt, t.location))
+                    rc.build(toBeBuilt, t.location);
+
+
             }
         } else {//there is no task to be done
             //There is no task to be done and all the flags have guys sitting on them
             //Move away from the nearest guys avoiding ops especicially
-            if(rc.getLocation().distanceSquaredTo(findClosestSpawnLocation(rc)) > 15){
+            if (rc.getLocation().distanceSquaredTo(findClosestSpawnLocation(rc)) > 15) {
                 Pathfinding.bugNav2(rc, findClosestSpawnLocation(rc));
             }
             Direction d = directionToMove(rc);
-            for(int i = 0;i<8;i++){
-                if(rc.canMove(d)){
+            for (int i = 0; i < 8; i++) {
+                if (rc.canMove(d)) {
                     rc.move(d);
                 }
                 d = d.rotateLeft();
             }
 
-            if(rc.getRoundNum()%8==0/*&&distanceFromNearestSpawnLocation(rc)>16*/)
+            if (rc.getRoundNum() % 8 == 0)
                 UpdateExplosionBorder(rc);
 
         }
@@ -106,14 +107,39 @@ public class Builder {
             rc.attack(toAttack);
     }
 
-
-    private static Direction directionToMove(RobotController rc) {
-        return directions[rng.nextInt(8)];
+    private static boolean ShouldBuildWater(RobotController rc) throws GameActionException {
+        int nearestFlag = findClosestSpawnLocation(rc).distanceSquaredTo(rc.getLocation());
+        int distanceFromNearestWall = distanceFromNearestSpawnWall(rc);
+        int minDistanceFromFlag = (rc.getMapHeight() + rc.getMapWidth()) / 2 - (rc.getMapHeight() + rc.getMapWidth()) / 3;
+        return (nearestFlag >= (minDistanceFromFlag) && distanceFromNearestWall > 5);
     }
 
-    private static int distanceFromNearestSpawnLocation(RobotController rc){
-return 0;
+    private static int distanceFromNearestSpawnWall(RobotController rc) {
+        int fromTop = rc.getMapHeight() - rc.getLocation().y;
+        int fromBottom = rc.getLocation().y;
+        int fromLeft = rc.getLocation().x;
+        int fromRight = rc.getMapWidth() - rc.getLocation().x;
+        int out = Math.min(fromTop, fromRight);
+        out = Math.min(out, fromLeft);
+        out = Math.min(out, fromBottom);
+        return out;
+
     }
+
+
+    private static Direction directionToMove(RobotController rc) throws GameActionException {
+        Direction d = directions[rc.getID() % 8];
+        MapLocation ops = Utilities.averageRobotLocation(rc.senseNearbyRobots(-1, rc.getTeam().opponent()));
+        if (ops != null)
+            for (int i = 0; i < 8; i++) {
+                if (ops.distanceSquaredTo(rc.getLocation()) > ops.distanceSquaredTo(rc.getLocation().add(d)))
+                    break;
+                d.rotateLeft();
+            }
+        return d;
+
+    }
+
 
     public static boolean locationIsActionable(RobotController rc, MapLocation m) throws GameActionException {
         MapInfo[] ActionableTiles = rc.senseNearbyMapInfos(GameConstants.INTERACT_RADIUS_SQUARED);
@@ -128,12 +154,18 @@ return 0;
 
     public static void UpdateExplosionBorder(RobotController rc) throws GameActionException {
         for (MapInfo t : rc.senseNearbyMapInfos(GameConstants.INTERACT_RADIUS_SQUARED)) {
-            if (rc.canBuild(TrapType.STUN, t.getMapLocation())&&t.getTrapType().equals(TrapType.NONE)) {
-                rc.build(TrapType.STUN, t.getMapLocation());
+            Pathfinding.bugNav2(rc, t.getMapLocation());
+            TrapType toBeBuilt = TrapType.STUN;
+            if (rc.getCrumbs() >= 3000)
+                toBeBuilt = TrapType.EXPLOSIVE;
+            if (locationIsActionable(rc, t.getMapLocation())) {
+                if (rc.canBuild(toBeBuilt, t.getMapLocation()))
+                    rc.build(toBeBuilt, t.getMapLocation());
+
             }
         }
-    }
 
+    }
 
 
 }
